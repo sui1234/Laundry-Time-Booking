a9e5ee0ddad3f7548115571d9cd16ad1e0470e09
Yujie Sui <rainjie87@gmail.com>
2019-03-13 17:09:52 +0100

    Layout mina bokingar sida

--- app/src/main/java/com/example/mytbooking/HomeFragment.java
+++ app/src/main/java/com/example/mytbooking/HomeFragment.java
@@ -13,5 +13,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+
+
     }
 }
--- app/src/main/java/com/example/mytbooking/MainActivity.java
+++ app/src/main/java/com/example/mytbooking/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.mytbooking;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
