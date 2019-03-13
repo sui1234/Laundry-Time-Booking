f30a4d0b63c7220c59ddacf9c6f2649880f156ea
Sui <rainjie87@gmail.com>
2019-03-13 21:33:46 +0100

    Framework added

--- app/src/main/java/com/example/mytbooking/HomeFragment.java
+++ app/src/main/java/com/example/mytbooking/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.mytbooking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View plusView = inflater.inflate(R.layout.fragment_home,null);
+        plusView.findViewById(R.id.fab)
+
+        .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity().getApplicationContext(),BookingActivity.class);
+                startActivity(intent);
+            }
+        });
+        return plusView;
 
 
     }
