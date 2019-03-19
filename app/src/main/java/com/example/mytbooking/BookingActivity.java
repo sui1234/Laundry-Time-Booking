b390b05da6ac92473f28fa68c80e5c271e3d8da9
Sui <rainjie87@gmail.com>
2019-03-19 12:55:09 +0100

    date single choice dialog is finished,

--- app/src/main/java/com/example/mytbooking/BookingActivity.java
+++ app/src/main/java/com/example/mytbooking/BookingActivity.java
@@ -1,62 +1,88 @@
 package com.example.mytbooking;
 
-import android.app.DatePickerDialog;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.RadioGroup;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-import java.util.Calendar;
-import java.util.Date;
 
 public class BookingActivity extends AppCompatActivity {
 
+    DatePicker date;
+    int choice;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
 
-        RadioGroup radioGroup = findViewById(R.id.radio_group);
+        date = findViewById(R.id.date_picker);
+        date.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Log.d("Sui", "dialog");
+                timeDialog();
 
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // checkedId is the RadioButton selected
-
-                switch (checkedId) {
-                    case R.id.button_date:
-                        Log.d("Sui", "mydatefragment");
-                       // DatePickerFragment datePickerFragment=new DatePickerFragment();
-                        //datePickerFragment.show(getSupportFragmentManager(),  "datepicker");
-
-
-
-
-                        replaceFragment(new DatePickerFragment());
-
-                        break;
-                    case R.id.button_time:
-
-                        Log.d("Sui", "mytimefragment");
-                        replaceFragment(new TimeFragment());
-                        break;
-                }
-            }
-
-            private void replaceFragment(Fragment fragment) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-                transaction.replace(R.id.fragment_container, fragment);
-                transaction.commit();
             }
         });
 
-
     }
+
+
+    private void timeDialog() {
+        final String[] items = {"07.00 - 11.00", "11.00 - 15.00", "15.00 - 19.00","19.00 - 23.00"};
+
+        AlertDialog.Builder timeDialog = new AlertDialog.Builder(BookingActivity.this);
+        timeDialog.setTitle("                         Välj tid");
+
+        timeDialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                choice = which;
+            }
+        });
+        timeDialog.setPositiveButton("Spara", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (choice != -1) {
+
+                    Intent intent = new Intent(BookingActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(BookingActivity.this,
+                            "Du har valt  " + items[choice],
+                            Toast.LENGTH_SHORT).show();
+
+
+
+                }
+            }
+        });
+
+        timeDialog.setNegativeButton("back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (choice != -1) {
+                    Toast.makeText(BookingActivity.this,
+                            "Du har lämnat tid dialog  ",
+                            Toast.LENGTH_SHORT).show();
+
+
+
+                }
+            }
+        });
+        timeDialog.show();
+    }
+
 
 }
 
